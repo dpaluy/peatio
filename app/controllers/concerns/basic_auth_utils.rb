721bc03425aaabed72edf7346f100cb553b7bcb2
David Paluy <david@paluy.org>
2018-04-13 13:28:11 +0300

    cleanup

--- app/controllers/concerns/basic_auth_utils.rb
+++ app/controllers/concerns/basic_auth_utils.rb
@@ -1,7 +1,7 @@
 module Concerns
   module BasicAuthUtils
     extend ActiveSupport::Concern
-    
+
     included do
       if ENV['BASIC_AUTH_LOGIN'].present?
         http_basic_authenticate_with name: ENV['BASIC_AUTH_LOGIN'], password: ENV['BASIC_AUTH_PASSWORD']
